8cf013ede7f96ea1e8333fba2a4725814c9077e5
Daniel Cloud <dcloud@sunlightfoundation.com>
2014-08-15 18:02:23 -0400

    Use requests_futures. Print out audit info.

--- app.py
+++ app.py
@@ -1,26 +1,54 @@
+#!/usr/bin/env python
+
 import csv
 import re
-import requests
 import json
+import os
+import hashlib
 
-def is_valid_url(url):
-    return re.match(r'https?://', url, re.I) is not None
+from thezombies import (AuditableRequest, AuditableResponse)
+from thezombies.utils import is_valid_url
+
+from concurrent.futures import ThreadPoolExecutor
+from requests_futures.sessions import FuturesSession
+
+session = FuturesSession(executor=ThreadPoolExecutor(max_workers=10))
+
+IMPORTANT_HEADERS = ('Agency', '/data' '/data.json', 'JSON Error?')
+
+TMP_SAVE = os.path.abspath('./tmp')
+
+class NotFoundError(Exception):
+    """ Raised when passing an search fails"""
+    pass
+
+# def find_object_with_url(url, objects):
+#     def url_in_values(item):
+#         return url in item.values()
+#     results = list(filter(url_in_values, objects))
+#     if len(results) is 1:
+#         return results[0]
+#     else:
+#         raise NotFoundError
+
+# def get_url_hash(url):
+#     return hashlib.sha1(url.encode('utf-8')).hexdigest()
+
 
 def main():
-    reader = csv.reader(open('open_data_data_inventory_audit.csv', 'r'))
-    for (i, line) in enumerate(reader):
-        if i==0:
-            continue
+    reader = csv.DictReader(open('open_data_data_inventory_audit.csv', 'r'))
+    audit_objects = list(reader)
 
-        data_json_url = line[5]
+    futures = []
+    for item in audit_objects[4:7]:
+        data_json_url = item.get('/data.json')
         if is_valid_url(data_json_url):
-            r = requests.get(data_json_url)
-            decoded_json = None
-            try:
-                decoded_json = json.loads(r.content)
-            except:
-                print 'HTTP %d: %s' % (r.status_code, data_json_url)
+            f = session.get(data_json_url)
+            futures.append(f)
 
+    for f in futures:
+        resp = AuditableResponse(f.result())
+        print(resp.audit())
 
 
 
