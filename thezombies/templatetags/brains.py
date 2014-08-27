8b70fda1b63ab9f6446745eb1c4846c84e452ab2
Daniel Cloud <dcloud@sunlightfoundation.com>
2014-08-27 19:17:06 -0400

    Fix truthiness handling of None (not False) values. Clean up agency page in prep for reports page.

--- thezombies/templatetags/brains.py
+++ thezombies/templatetags/brains.py
@@ -7,5 +7,9 @@ register = template.Library()
 @stringfilter
 def truthy(value, arg=None):
     """Wraps django's yesno filter to allow for JavaScript-style true or false string values."""
-    is_true = (value.lower() == 'true')
-    return yesno(is_true, arg)
+    truthiness = None
+    if value.lower() == 'true':
+        truthiness = True
+    elif value.lower() == 'false':
+        truthiness = False
+    return yesno(truthiness, arg)
