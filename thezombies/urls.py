a8508e7e8f994b114b785c6024c43d9a2199a543
Daniel Cloud <dcloud@sunlightfoundation.com>
2014-09-03 19:08:07 -0400

    Add a ReportURLList view, url, and template. Make some reusable template snippets.

--- thezombies/urls.py
+++ thezombies/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-from thezombies.views import (AgencyList, AgencyView, ReportList, ReportView)
+from thezombies.views import (AgencyList, AgencyView, ReportList, ReportView, ReportURLList)
 
 urlpatterns = patterns('',
     # Examples:
@@ -9,6 +9,7 @@ urlpatterns = patterns('',
     url(r'^agency/(?P<slug>[\w\-]+)$', AgencyView.as_view(), name='agency-detail'),
     url(r'^reports/$', ReportList.as_view(), name='reports-list'),
     url(r'^reports/(?P<pk>\d+)$', ReportView.as_view(), name='report-detail'),
+    url(r'^reports/(?P<pk>\d+)/urls$', ReportURLList.as_view(), name='report-url-list'),
 
     url(r'^admin/', include(admin.site.urls)),
 )
--- thezombies/views.py
+++ thezombies/views.py
@@ -1,6 +1,7 @@
+from django.shortcuts import get_object_or_404
 from django.views.generic import ListView, DetailView
 
-from thezombies.models import (Agency, Report)
+from thezombies.models import (Agency, Report, URLResponse)
 
 
 class AgencyList(ListView):
@@ -23,3 +24,17 @@ class ReportView(DetailView):
     model = Report
     context_object_name = 'report'
     template_name = 'report_detail.html'
+
+class ReportURLList(ListView):
+    context_object_name = 'responses'
+    paginate_by = 50
+    template_name = 'report_url_list.html'
+
+    def get_queryset(self):
+        self.report = get_object_or_404(Report, pk=self.kwargs.get('pk'))
+        return URLResponse.objects.filter(report=self.report)
+
+    def get_context_data(self, **kwargs):
+        context = super(ReportURLList, self).get_context_data(**kwargs)
+        context['report'] = self.report
+        return context
