de46b76e398407308ac535f04612a34d96c8c147
Daniel Cloud <dcloud@sunlightfoundation.com>
2014-09-04 15:00:41 -0400

    Trailing slashes on urls.

--- thezombies/urls.py
+++ thezombies/urls.py
@@ -6,9 +6,9 @@ from thezombies.views import (AgencyList, AgencyView, ReportList, ReportView, Re
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', AgencyList.as_view(), name='agency-list'),
-    url(r'^agency/(?P<slug>[\w\-]+)$', AgencyView.as_view(), name='agency-detail'),
+    url(r'^agency/(?P<slug>[\w\-]+)/$', AgencyView.as_view(), name='agency-detail'),
     url(r'^reports/$', ReportList.as_view(), name='reports-list'),
-    url(r'^reports/(?P<pk>\d+)$', ReportView.as_view(), name='report-detail'),
+    url(r'^reports/(?P<pk>\d+)/$', ReportView.as_view(), name='report-detail'),
     url(r'^reports/(?P<pk>\d+)/urls$', ReportURLList.as_view(), name='report-url-list'),
 
     url(r'^admin/', include(admin.site.urls)),
