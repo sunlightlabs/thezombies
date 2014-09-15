4baa7113fb1ddd31371a70d2f2c08cfdb69aadaf
Daniel Cloud <dcloud@sunlightfoundation.com>
2014-09-15 14:18:06 -0400

    Filter probes for catalog item probes that have no url in the proper places.

--- thezombies/management/commands/catalog_crawl_stats.py
+++ thezombies/management/commands/catalog_crawl_stats.py
@@ -1,5 +1,6 @@
+from django.db.models import Q
 from django.core.management.base import BaseCommand
-from thezombies.models import (Audit, Agency)
+from thezombies.models import (Audit, Agency, Probe)
 from optparse import make_option
 
 
@@ -34,12 +35,19 @@ class Command(BaseCommand):
                 ftp_inspections = typeless_inspections.filter(requested_url__startswith='ftp')
                 typeless_http_inspections = typeless_inspections.filter(requested_url__startswith='http')
                 other_typeless_inspections = typeless_inspections.exclude(id__in=ftp_inspections).exclude(id__in=typeless_http_inspections)
+                json_probes = audit.probe_set.filter(probe_type=Probe.JSON_PROBE, previous__isnull=False)
+                json_probes_public = json_probes.filter(initial__contains={u'accessLevel': u'public'})
+                has_distribution_q = Q(initial__contains=['distribution'])
+                has_accessURL_q = Q(initial__contains=['accessURL'])
+                has_accessURL_badkey_q = Q(initial__contains=['accessUrl'])
+                has_webservice_q = Q(initial__contains=['webService'])
+                sans_data_urls = json_probes_public.exclude(has_distribution_q | has_accessURL_q | has_accessURL_badkey_q | has_webservice_q)
                 self.stdout.write(u"\nUpdated: {0}\n".format(date_str))
-                self.stdout.write(u"| Errors | URLs inspected | URLs no content-type | FTP URLs | Possibly Invalid URLs |")
-                self.stdout.write(u"| ------ | -------------- | -------------------- | -------- | --------------------- |")
-                stats_row = u"| {errors:6,d} | {inspected: >14,d} | {typeless:20,d} | {ftp:8,d} | {invalid:21,d} |".format(
+                self.stdout.write(u"| Errors | Entries sans urls | URLs inspected | URLs no content-type | FTP URLs | Possibly Invalid URLs |")
+                self.stdout.write(u"| ------ | ----------------- | -------------- | -------------------- | -------- | --------------------- |")
+                stats_row = u"| {errors:6,d} | {sans_urls: >14,d} | {inspected: >14,d} | {typeless:20,d} | {ftp:8,d} | {invalid:21,d} |".format(
                             errors=audit.error_count(), inspected=num_urls_visited, typeless=typeless_http_inspections.count(),
-                            ftp=ftp_inspections.count(), invalid=other_typeless_inspections.count())
+                            ftp=ftp_inspections.count(), invalid=other_typeless_inspections.count(), sans_urls=sans_data_urls.count())
                 self.stdout.write(stats_row)
                 selector = u'content__content_type'
                 insp_content_types = insp_list.select_related(selector).order_by(selector).distinct(selector).only(selector)
