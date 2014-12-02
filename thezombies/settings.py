53eaf1a1a74fead59a8c48b30478ccff3c3bffe8
Daniel Cloud <dcloud@sunlightfoundation.com>
2014-12-02 10:56:57 -0500

    Match heroku settings for redis url.

--- thezombies/settings.py
+++ thezombies/settings.py
@@ -107,8 +107,8 @@ MEDIA_URL = os.getenv('MEDIA_URL', '/media/')
 
 # Celery
 
-BROKER_URL = os.getenv('BROKER_URL', "redis://localhost:6379/0")
-CELERY_RESULT_BACKEND = os.getenv('CELERY_RESULT_BACKEND', "redis://localhost:6379/1")
+BROKER_URL = os.getenv('REDISGREEN_URL', "redis://localhost:6379/0")
+CELERY_RESULT_BACKEND = os.getenv('REDISGREEN_URL', "redis://localhost:6379/1")
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_DISABLE_RATE_LIMITS = True
 CELERYD_TASK_TIME_LIMIT = 10 * 60
