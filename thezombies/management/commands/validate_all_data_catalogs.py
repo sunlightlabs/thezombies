79e23159c308a69896c464eda13c043dbbc8086e
Daniel Cloud <dcloud@sunlightfoundation.com>
2014-12-03 12:10:56 -0500

    Fix options on NoArgsCommand. Huh.

--- thezombies/management/commands/validate_all_data_catalogs.py
+++ thezombies/management/commands/validate_all_data_catalogs.py
@@ -5,6 +5,6 @@ from thezombies.tasks.main import validate_data_catalogs
 class Command(NoArgsCommand):
     """Validate all of the agency data catalogs"""
 
-    def handle_noargs(self):
+    def handle_noargs(self, **options):
         validator_group = validate_data_catalogs.delay()
         self.stdout.write(u"\nSpawned data catalog task group: {0}\n".format(validator_group.id))
