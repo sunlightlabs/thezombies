227d92e8c24daaf8e6739205b161a30a6827c6d9
Daniel Cloud <dcloud@sunlightfoundation.com>
2014-12-18 18:32:34 -0500

    Rework some views, split out a table for audit urls into an includable template. A few of those views are more efficient, others maybe a little.

--- thezombies/urls.py
+++ thezombies/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 from thezombies.views import (HomeView, AgencyList, AgencyView, AuditListView, AuditView, AuditURLList,
-                              AuditDayArchiveView, AuditMonthArchiveView, AuditYearArchiveView)
+                              AuditDayArchiveView, AuditMonthArchiveView, AuditYearArchiveView, ProbeView)
 
 urlpatterns = patterns('',
     url(r'^$', HomeView.as_view(), name='home'),
@@ -15,6 +15,7 @@ urlpatterns = patterns('',
     url(r'^audits/(?P<pk>\d+)/$', AuditView.as_view(), name='audit-detail'),
     url(r'^audits/(?P<pk>\d+)/urls/$', AuditURLList.as_view(), name='audit-url-list'),
     url(r'^audits/(?P<audit_type>\w+)/$', AuditListView.as_view(), name='audits-list-filtered'),
+    url(r'^probes/(?P<pk>\d+)/$', ProbeView.as_view(), name='probe-detail'),
 
     url(r'^admin/', include(admin.site.urls)),
 )
--- thezombies/views.py
+++ thezombies/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import get_object_or_404, get_list_or_404
+from django.shortcuts import get_object_or_404
 from django.http import Http404
 from django.views.generic import ListView, DetailView
+from django.views.generic.detail import SingleObjectMixin
 from django.views.generic.base import RedirectView
 from django.views.generic.dates import DayArchiveView, MonthArchiveView, YearArchiveView
 
-from thezombies.models import (Agency, Audit)
+from thezombies.models import (Agency, Audit, Probe, URLInspection)
 
 
 class HomeView(RedirectView):
@@ -68,7 +69,7 @@ class AuditListView(ListView):
                 audit_filter_kwargs['audit_type'] = Audit.DATA_CATALOG_CRAWL
             else:
                 raise Http404
-        self.audit_list = Audit.objects.filter(**audit_filter_kwargs)
+        self.audit_list = Audit.objects.filter(**audit_filter_kwargs).prefetch_related('agency')
         return self.audit_list
 
     def get_context_data(self, **kwargs):
@@ -77,9 +78,39 @@ class AuditListView(ListView):
         return context
 
 
-class AuditView(DetailView):
-    model = Audit
+AUDIT_TYPE_TEMPLATES = (
+    (Audit.GENERIC_AUDIT, 'audit_detail.html'),
+    (Audit.DATA_CATALOG_CRAWL, 'audit_crawl_detail.html'),
+    (Audit.DATA_CATALOG_VALIDATION, 'audit_validation_detail.html'),
+)
+
+
+class AuditView(SingleObjectMixin, ListView):
+    paginate_by = 20
     template_name = 'audit_detail.html'
+
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object(queryset=Audit.objects.all())
+        return super(AuditView, self).get(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super(AuditView, self).get_context_data(**kwargs)
+        context['audit'] = self.object
+        return context
+
+    def get_queryset(self):
+        url_inspections = URLInspection.objects.filter(probe__in=self.object.probe_set.all())
+        # url_inspections.prefetch_related('content__content_type').defer('content__binary')
+        # url_inspections.defer('probe__initial', 'probe__result')
+        return url_inspections
+
+    def get_template_names(self):
+        template_names = super(AuditView, self).get_template_names()
+        template_choices = dict(AUDIT_TYPE_TEMPLATES)
+        template_addition = template_choices.get(self.object.audit_type, None) if self.object else None
+        if template_addition:
+            template_names.insert(0, template_addition)
+        return template_names
 
 
 class AuditURLList(ListView):
@@ -88,9 +119,17 @@ class AuditURLList(ListView):
 
     def get_queryset(self):
         self.audit = get_object_or_404(Audit, pk=self.kwargs.get('pk'))
-        return self.audit.url_inspections
+        # probe_set = self.audit.probe_set.all()
+        # probe_set.only('content__content_type', 'probe__errors', 'info')
+        url_inspections = URLInspection.objects.filter(probe__in=probe_set)
+        return url_inspections
 
     def get_context_data(self, **kwargs):
         context = super(AuditURLList, self).get_context_data(**kwargs)
         context['audit'] = self.audit
         return context
+
+
+class ProbeView(DetailView):
+    model = Probe
+    template_name = "probe_detail.html"
