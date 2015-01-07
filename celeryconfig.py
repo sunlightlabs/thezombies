09b2b4ecd2ad869a47e2931b18cb57e5fb4bcb2b
Daniel Cloud <dcloud@sunlightfoundation.com>
2015-01-07 16:00:37 -0500

    Stick to pickle because we may want to pass data that doesn't compress to json.

--- celeryconfig.py
+++ celeryconfig.py
@@ -1,9 +1,10 @@
 import os
 
 # Basic config
+# IMPORTANT: Using pickle exclusively because we are passing objects that are potentially invalid JSON.
 BROKER_URL = os.getenv('CLOUDAMQP_URL', 'amqp://localhost')
 CELERY_DEFAULT_RATE_LIMIT = '100/s'
-CELERY_ACCEPT_CONTENT = ['pickle', 'json']
+CELERY_ACCEPT_CONTENT = ['pickle']
 CELERY_MESSAGE_COMPRESSION = 'gzip'
 # CELERY_DISABLE_RATE_LIMITS = True
 # Tasks
@@ -11,5 +12,5 @@ CELERY_TASK_SERIALIZER = 'pickle'
 CELERYD_TASK_TIME_LIMIT = 10 * 60
 # Results
 CELERY_TASK_RESULT_EXPIRES = 7200  # 2 hours.
-CELERY_RESULT_BACKEND = os.getenv('REDISTOGO_URL', 'redis://')
-CELERY_RESULT_SERIALIZER = 'json'
+CELERY_RESULT_BACKEND = os.getenv('CLOUDAMQP_URL', 'amqp://localhost')
+CELERY_RESULT_SERIALIZER = 'pickle'
