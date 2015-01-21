b164c16a861159a9ff02f4e5cd12c42d9b32bbde
Daniel Cloud <dcloud@sunlightfoundation.com>
2015-01-21 13:13:02 -0500

    Change celeryconfig.py defaults for BROKER_URL and CELERY_RESULT_BACKEND. Set defaults for both for the workers vm ip.

--- celeryconfig.py
+++ celeryconfig.py
@@ -2,7 +2,7 @@ import os
 
 # Basic config
 # IMPORTANT: Using pickle exclusively because we are passing objects that are potentially invalid JSON.
-BROKER_URL = os.getenv('RABBITMQ_BIGWIG_TX_URL', 'amqp://10.64.7.102:5672//')
+BROKER_URL = os.getenv('BROKER_URL', 'amqp://10.64.7.102:5672//')
 CELERY_DEFAULT_RATE_LIMIT = '100/s'
 CELERY_ACCEPT_CONTENT = ['pickle']
 CELERY_MESSAGE_COMPRESSION = 'gzip'
@@ -12,5 +12,5 @@ CELERY_TASK_SERIALIZER = 'pickle'
 CELERYD_TASK_TIME_LIMIT = 10 * 60
 # Results
 CELERY_TASK_RESULT_EXPIRES = 7200  # 2 hours.
-CELERY_RESULT_BACKEND = os.getenv('RABBITMQ_BIGWIG_RX_URL', 'amqp://10.64.7.102:5672//')
+CELERY_RESULT_BACKEND = os.getenv('CELERY_RESULT_BACKEND', 'amqp://10.64.7.102:5672//')
 CELERY_RESULT_SERIALIZER = 'pickle'
